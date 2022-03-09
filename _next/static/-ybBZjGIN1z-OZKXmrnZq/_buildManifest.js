a288d396a97685729c7c5a9cb29af16e2109d280
yunu7067 <yunu7067@gmail.com>
2022-03-09 22:51:13 +0900

    update

---
+++ _next/static/-ybBZjGIN1z-OZKXmrnZq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6fb556a0ae0f0891.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/p/[slug]":["static/chunks/pages/p/[slug]-7ea19ce6d3295bb5.js"],sortedPages:["/","/_app","/_error","/p/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
